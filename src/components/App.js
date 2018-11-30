7d3ff560c913b8abc38886a5afc3eaefb1bc45bc
nikolay.okunev <nikolajs.okunevs@gmail.com>
2018-11-30 20:00:46 +0200

    ok: add validation for login screen

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends Component {
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>!</h2>
+                    <h2>Best Planner Ever!</h2>
                 </div>
                 <FormLogin />
             </div>
