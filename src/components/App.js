c766fed089f611b5f6b9e6305e53c9a5ae161a44
mhakimov <xakim@inbox.lv>
2019-04-08 21:08:11 +0100

    create account functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import Login from "./Login";
+import CreateAccountComponent from "./CreateAccountComponent";
+import ReactDOM from "react-dom";
 
 class App extends Component {
+  navigateToRegistrationPage() {
+    ReactDOM.render(
+      <CreateAccountComponent />,
+      document.getElementById("root")
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <Login />
+        <button
+          className="btn btn-secondary m-2"
+          onClick={this.navigateToRegistrationPage}
+        >
+          Create Account
+        </button>
       </div>
     );
   }
